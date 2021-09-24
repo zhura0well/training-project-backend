9a6d7e8453be2380ae17b09ca86deb26aea91abd
Slava <slavonzhuravel@gmail.com>
2021-09-24 13:09:06 +0300

    errors fixed

--- server.js
+++ server.js
@@ -25,11 +25,9 @@ app.use(authRoutes)
 
 
 if (process.env.NODE_ENV === 'production') {
-
-    app.use(express.static(path.join(__dirname, '../client/build')))
-    
+    app.use(express.static(path.resolve('../client/build')))
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
+        res.sendFile(path.resolve('../client/build', 'index.html'))
     })
 }
 
