04740fdfc26485c5757cacec19cef9e8ea71cdcc
Slava <slavonzhuravel@gmail.com>
2021-09-27 13:32:11 +0300

    attribute 'path' added in res.cookie

--- routes/auth.js
+++ routes/auth.js
@@ -19,10 +19,10 @@ router.post('/api/register', async (req, res) => {
         const user = new Auth({ username, password: hashPassword, roles: [ROLE.USER] })
         await user.save()
 
-        const token = jwt.sign({ id: user._id, roles: user.roles }, jwtKey, { expiresIn: '1h' })
+        const token = jwt.sign({ id: user._id, roles: user.roles }, jwtKey, { expiresIn: '5h' })
 
-        res.cookie('jwt', token, { httpOnly: true })
-        res.cookie('roles', user.roles.join('|'))
+        res.cookie('jwt', token, { httpOnly: true, sameSite: 'none', secure: true, path: '/' })
+        res.cookie('roles', user.roles.join('|'), { sameSite: 'none', secure: true, path: '/' })
 
         res.status(201).json({ jwt: token, roles: user.roles })
 
@@ -48,8 +48,8 @@ router.post('/api/login', async (req, res) => {
 
         const token = jwt.sign({ id: user._id, roles: user.roles }, jwtKey, { expiresIn: '5h' })
 
-        res.cookie('jwt', token, { httpOnly: true, sameSite: 'none', secure: true })
-        res.cookie('roles', user.roles.join('|'), { sameSite: 'none', secure: true })
+        res.cookie('jwt', token, { httpOnly: true, sameSite: 'none', secure: true, path: '/' })
+        res.cookie('roles', user.roles.join('|'), { sameSite: 'none', secure: true, path: '/' })
 
         res.status(200).json({ jwt: token, roles: user.roles })
     } catch (e) {
