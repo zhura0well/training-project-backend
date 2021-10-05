0321f084881a08253ff94a74e830d36c98165e4c
Slava <slavonzhuravel@gmail.com>
2021-10-05 15:03:28 +0300

    quantity added

--- models/Products.js
+++ models/Products.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose'
 const schema = new mongoose.Schema({
     title: {type: String, required: true, unique: true},
     description: {type: String, required: true},
-    price: {type: Number, required: true},
+    price: {type: Number, required: true, default: 0},
+    quantity: {type: Number, required: true, default: 0}
     /*image: {type: String} not sure*/
 })
 
