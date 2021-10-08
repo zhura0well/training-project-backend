8edb38dd10648c4238e04152f43d2901fcb1f003
Slava <slavonzhuravel@gmail.com>
2021-10-08 15:09:45 +0300

    done, need to test how it works on heroku

--- routes/images.js
+++ routes/images.js
@@ -1,29 +1,73 @@
 import { Router } from 'express'
 import multer from 'multer'
-import { uploadFile } from '../aws_s3.js'
-const router = Router()
+import fs from 'fs'
+import util from 'util'
+import Products from '../models/Products.js'
+import AWS from 'aws-sdk'
+import dotenv from 'dotenv'
+dotenv.config()
 
-const upload = multer({ dest: 'uploads/'})
-router.post('/api/images', upload.single('file'), async (req, res) => {
+const bucketName = process.env.AWS_BUCKET_NAME
+const region = process.env.AWS_BUCKET_REGION
+const accessKeyId = process.env.AWS_ACCESS_KEY
+const secretAccessKey = process.env.AWS_SECRET_KEY
+
+const s3 = new AWS.S3({
+    region,
+    accessKeyId,
+    secretAccessKey
+})
+
+function uploadFile(file) {
+    const fileStream = fs.createReadStream(file.path)
+
+    const uploadParams = {
+        Bucket: bucketName,
+        Body: fileStream,
+        Key: file.filename
+    }
+
+    return s3.upload(uploadParams).promise()
+}
+
+
+function getFileStream(fileKey) {
+    const downloadParams = {
+        Key: fileKey,
+        Bucket: bucketName
+    }
+
+    return s3.getObject(downloadParams).createReadStream()
+}
+
+const upload = multer({ dest: 'uploads/' })
+
+
+router.put('/api/images/:productId', upload.single('file'), async (req, res) => {
     try {
         const file = req.file
-        console.log(file)
+
         const result = await uploadFile(file)
-        console.log(result.Location)
-        return res.status(201).json({ image: result.Location })
+
+        const product = await Products.findByIdAndUpdate(req.params.productId, { imageKey: result.key }, { new: true })
+
+        await unlinkFile(file.path)
+        return res.status(200).json(product)
     } catch (e) {
         console.log(e)
         res.status(400).json({ message: 'Error occured' })
     }
 })
 
-router.get('/api/images', upload.single('file'), async (req, res) => {
+router.get('/api/images/:productId', upload.single('file'), async (req, res) => {
     try {
-        const file = req.file
-        console.log(file)
-        const result = await uploadFile(file)
-        console.log(result.Location)
-        return res.status(201).json({ image: result.Location })
+
+        const product = await Products.findById(req.params.productId)
+        const key = product.imageKey
+
+        const readStream = getFileStream(key)
+
+        readStream.pipe(res)
     } catch (e) {
         console.log(e)
         res.status(400).json({ message: 'Error occured' })
